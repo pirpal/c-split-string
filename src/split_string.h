497c08099e000275717ccb6b3caa442a13e247dd
pirpal <ppaillere@gmail.com>
2022-07-03 04:27:21 +0200

    NULL terminated split

--- src/split_string.h
+++ src/split_string.h
@@ -7,21 +7,13 @@
 #include <stdint.h>
 #include <string.h>
 
-//void err_exit(const char *msg);
-
-//uint16_t count_sep(const char *str, const char sep);
-
-//uint16_t *sep_indexes(const char *str, const char sep);
-
-//uint16_t *tokens_lengths(const char *str, const char sep);
-
 uint16_t tokens_nb(const char *str, const char sep);
 
 char **new_split(const char *str, const char sep);
 
-void free_split(char **split, uint16_t tok_nb);
+void free_split(char **split);
 
-void log_split(FILE *stream, char **split, uint16_t tok_nb);
+void log_split(FILE *stream, char **split);
 
 
 #endif // SPLIT_STRING_H_
